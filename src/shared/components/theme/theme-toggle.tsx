7ce9fac82680a2d322ba36757b320eada21d6bae
Régi GOUALE <regi.gouale@gmail.com>
2025-01-18 03:06:31 +0100

    Renommer le composant ThemeToggle et améliorer la gestion de l'état du thème

--- src/shared/components/theme/theme-toggle.tsx
+++ src/shared/components/theme/theme-toggle.tsx
@@ -3,18 +3,28 @@
 import { Switch } from "@ui/switch";
 import { MoonIcon, SunIcon } from "lucide-react";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export function ThemeToggle() {
   const { theme, setTheme } = useTheme();
+
+  const [isDark, setIsDark] = useState(false);
+
+  useEffect(() => {
+    setIsDark(theme === "dark");
+  }, [theme]);
+
   const toggleTheme = () => {
     setTheme(theme === "light" ? "dark" : "light");
+    setIsDark(!isDark);
   };
+  console.log("theme", theme);
 
   return (
     <div className="flex items-center space-x-2">
       <SunIcon className="size-6" />
       <Switch
-        checked={theme === "dark"}
+        checked={isDark}
         onCheckedChange={toggleTheme}
         aria-label="Toggle theme"
       />
