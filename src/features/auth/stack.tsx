c79a34305531014e51dc754e003fb07c30a92874
Régi GOUALE <regi.gouale@gmail.com>
2025-01-20 08:18:19 +0100

    Suppression des fichiers d'authentification et de la page d'accueil, ajout de la gestion du stack server et d'un nouveau layout pour le gestionnaire

---
+++ src/features/auth/stack.tsx
@@ -0,0 +1,11 @@
+import "server-only";
+
+import { StackServerApp } from "@stackframe/stack";
+
+export const stackServerApp = new StackServerApp({
+  tokenStore: "nextjs-cookie",
+  urls: {
+    afterSignIn: process.env.NEXT_PUBLIC_AFTER_SIGN_IN_URL || "/dashboard",
+    afterSignOut: process.env.NEXT_PUBLIC_AFTER_SIGN_OUT_URL || "/",
+  },
+});
